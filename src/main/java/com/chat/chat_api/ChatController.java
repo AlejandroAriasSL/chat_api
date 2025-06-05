c14dc04dd640a2e4e966294dd7083d75453f19fb
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-05 14:46:09 +0200

    Feature: add basic ChatController

---
+++ src/main/java/com/chat/chat_api/ChatController.java
@@ -0,0 +1,16 @@
+package com.chat.chat_api;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+
+    public Message send(Message message) {
+        return new Message(message.timestamp(), message.content(), message.senderUsername());
+    }
+}
