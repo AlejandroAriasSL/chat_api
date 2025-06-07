c7e3da948341f4e8fb9ec778272b8de0f55bc8b1
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-07 22:04:31 +0200

    Feature: Add ChatService (WIP)

---
+++ src/main/java/com/chat/chat_api/chatroom/ChatService.java
@@ -0,0 +1,37 @@
+package com.chat.chat_api.chatroom;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ChatService {
+
+    private final ChatroomRepository repository;
+
+    public ChatService(final ChatroomRepository repository){
+        this.repository = repository;
+    }
+
+    @Transactional
+    public Chatroom createOrUpdate(Chatroom chat){
+        return repository.save(chat);
+    }
+
+    public Chatroom getById(Long id) throws RuntimeException {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Chat not found!"));
+    }
+
+    public List<Chatroom> getAll(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) throws RuntimeException{
+        if (!repository.existsById(id)){
+            throw new RuntimeException("Chat doesn't exist!");
+        }
+        repository.deleteById(id);
+    }
+}
