2da782c63da7537c59a3da98c21b137ffe8ce9f8
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-07 22:21:32 +0200

    Feature: Add UserService(WIP)

---
+++ src/main/java/com/chat/chat_api/user/UserService.java
@@ -0,0 +1,39 @@
+package com.chat.chat_api.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(final UserRepository repository){
+        this.repository = repository;
+    }
+
+    @Transactional
+    public User createOrUpdate(User user){
+       return repository.save(user); 
+    }
+
+    public User getById(Long id) throws RuntimeException {
+        return repository.findById(id)
+                         .orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+
+    public List<User> getAll(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) throws RuntimeException {
+        if (!repository.existsById(id)){
+            throw new RuntimeException("User doesn't exist!");
+        }
+        repository.deleteById(id);
+    }
+}
