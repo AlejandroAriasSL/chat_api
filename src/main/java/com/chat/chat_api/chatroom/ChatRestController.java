dfb458a234992ba667c71c3438ff9b9625fa30be
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-08 23:37:44 +0200

    Feature: add ChatRestController (WIP)

---
+++ src/main/java/com/chat/chat_api/chatroom/ChatRestController.java
@@ -0,0 +1,31 @@
+package com.chat.chat_api.chatroom;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/chats")
+public class ChatRestController {
+
+    private final ChatService chatService;
+
+    public ChatRestController(ChatService chatService){
+        this.chatService = chatService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Chatroom> createChat(@RequestBody Chatroom chat){
+        return ResponseEntity.ok(chatService.createOrUpdate(chat)); 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Chatroom>> getAllChats(){
+        return ResponseEntity.ok(chatService.getAll());
+    }
+}
