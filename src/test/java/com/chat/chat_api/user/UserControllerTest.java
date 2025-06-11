3c286a1a2105938b3e70909ac6fef1fe456a5a2c
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-12 00:30:16 +0200

    Test: add UserController creates chat with user test

--- src/test/java/com/chat/chat_api/user/UserControllerTest.java
+++ src/test/java/com/chat/chat_api/user/UserControllerTest.java
@@ -2,9 +2,12 @@ package com.chat.chat_api.user;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
+import com.chat.chat_api.chatroom.Chatroom;
+import com.chat.chat_api.chatroom.dto.ChatDTO;
+import com.chat.chat_api.chatroom.dto.CreateChatRequestDTO;
 import com.chat.chat_api.user.dto.CreateUserRequestDTO;
 import com.chat.chat_api.user.dto.UserDTO;
 import com.chat.chat_api.utils.UserTestContext;
@@ -43,5 +49,26 @@ public class UserControllerTest {
 
         ResponseEntity<UserDTO> response = controller.createUser(createUserRequest);
         assertThat(response.getStatusCode(), is(equalTo(HttpStatusCode.valueOf(201))));
+    }
+
+    @Test
+    @DisplayName("UserController creates chat with user")
+    void test_controller_creates_chat_with_user(){
+
+        Long userId = 1L; 
+        User mockUser = context.createMockUser("Usuario1", userId);
+        
+        String chatName = "mockChat";
+        Chatroom mockChat = new Chatroom(chatName, 1L);
+
+        mockUser.getChats().add(mockChat);
+
+        when(context.getUserService().createChatWithUser(userId, chatName)).thenReturn(UserDTO.toDto(mockUser));
+        
+        ResponseEntity<UserDTO> response = controller.createChatWithUser(mockUser.getId(), mockChat.getName());
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatusCode.valueOf(200))));
+        assertThat(response.getBody().userId(), is(equalTo(mockUser.getId())));
+        assertThat(response.getBody().chatIds(), hasItem(mockChat.getId()));
     }
 }
