35f72f5a95fb272cc49f11bb9402cc3a35eba25a
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-13 14:00:38 +0200

    Chore: remove unused imports in Chat and User controllers

--- src/test/java/com/chat/chat_api/chatroom/ChatControllerTest.java
+++ src/test/java/com/chat/chat_api/chatroom/ChatControllerTest.java
@@ -2,7 +2,6 @@ package com.chat.chat_api.chatroom;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
--- src/test/java/com/chat/chat_api/user/UserControllerTest.java
+++ src/test/java/com/chat/chat_api/user/UserControllerTest.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.when;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
