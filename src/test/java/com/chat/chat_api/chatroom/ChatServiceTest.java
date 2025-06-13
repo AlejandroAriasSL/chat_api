1033416a26f0ddafba501e2d477fae5dd64c867e
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-13 10:40:45 +0200

    Test: add ChatService returns chat by id test

--- src/test/java/com/chat/chat_api/chatroom/ChatServiceTest.java
+++ src/test/java/com/chat/chat_api/chatroom/ChatServiceTest.java
@@ -7,6 +7,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +18,9 @@ import com.chat.chat_api.chatroom.dto.CreateChatRequestDTO;
 @DisplayName("ChatService unit tests")
 public class ChatServiceTest {
     
+    private ChatroomRepository repository = mock(ChatroomRepository.class);
+    private ChatService chatService = new ChatService(repository);
+
     @Test
     @DisplayName("ChatService correctly saves chat and returns DTO")
     void test_chatService_correctly_saves_chat(){
@@ -23,14 +28,27 @@ public class ChatServiceTest {
         String chatName = "Chat1";
         ChatDTO expectedResult = ChatDTO.toDTO(new Chatroom(chatName, 1L));
 
-        ChatroomRepository repository = mock(ChatroomRepository.class);
-        ChatService chatService = new ChatService(repository);
-
         CreateChatRequestDTO createChatRequest = new CreateChatRequestDTO(chatName);
         when(repository.save(any(Chatroom.class))).thenReturn(new Chatroom(chatName, 1L));
 
         ChatDTO actualResult = chatService.createOrUpdate(createChatRequest);
         assertThat(actualResult, is(equalTo(expectedResult)));
+
+    }
+
+    @Test
+    @DisplayName("ChatService returns chat by id")
+    void test_ChatService_returns_chat_by_id(){
+
+        String chatName = "Chat1";
+        CreateChatRequestDTO createChatRequestDTO = new CreateChatRequestDTO(chatName);
+        Chatroom savedChat = new Chatroom(chatName, 1L);
+
+        when(repository.save(any(Chatroom.class))).thenReturn(savedChat);
+        when(repository.findById(savedChat.getId())).thenReturn(Optional.of(savedChat));
+
+        chatService.createOrUpdate(createChatRequestDTO);
+        assertThat(chatService.getById(savedChat.getId()), is(equalTo(savedChat)));
 
     }
 }
