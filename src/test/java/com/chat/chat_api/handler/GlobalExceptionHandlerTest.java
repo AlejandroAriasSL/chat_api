f0bece3c70e9ed35126d94113b0b63e5d82d33c9
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-13 15:08:25 +0200

    Test: add GlobalExceptionHandler returns error response when ChatNotfoundException is thrown test

--- src/test/java/com/chat/chat_api/handler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/chat/chat_api/handler/GlobalExceptionHandlerTest.java
@@ -3,15 +3,13 @@ package com.chat.chat_api.handler;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.chat.chat_api.chatroom.exception.ChatNotFoundException;
 import com.chat.chat_api.handler.dto.ErrorResponse;
 import com.chat.chat_api.user.exception.UserNotFoundException;
 
@@ -19,7 +17,7 @@ import com.chat.chat_api.user.exception.UserNotFoundException;
 public class GlobalExceptionHandlerTest {
     
     @Test
-    @DisplayName("GlobalExceptionHanlder returns error response when UserNotFoundException is thrown")
+    @DisplayName("GlobalExceptionHandler returns error response when UserNotFoundException is thrown")
     void test_returns_errorResponse_when_UserNotFoundException(){
 
         GlobalExceptionHandler exceptionHandler = new GlobalExceptionHandler();
@@ -29,6 +27,22 @@ public class GlobalExceptionHandlerTest {
         ErrorResponse expectedErrorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), userNotFoundException.getMessage()); 
         
         ResponseEntity<ErrorResponse> response = exceptionHandler.handleUserNotFound(userNotFoundException);
+
+        assertThat(response.getStatusCode().is4xxClientError(), is(equalTo(true)));
+        assertThat(response.getBody(), is(equalTo(expectedErrorResponse)));
+    }
+
+    @Test
+    @DisplayName("GlobalExceptionHandler returns error response when ChatNotfoundException is thrown")
+    void test_returns_errorResponse_when_ChatNotFoundException(){
+
+        GlobalExceptionHandler exceptionHandler = new GlobalExceptionHandler();
+
+        ChatNotFoundException chatNotFoundException = new ChatNotFoundException(1L);
+
+        ErrorResponse expectedErrorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), chatNotFoundException.getMessage());
+
+        ResponseEntity<ErrorResponse> response = exceptionHandler.handleUserNotFound(chatNotFoundException);
 
         assertThat(response.getStatusCode().is4xxClientError(), is(equalTo(true)));
         assertThat(response.getBody(), is(equalTo(expectedErrorResponse)));
