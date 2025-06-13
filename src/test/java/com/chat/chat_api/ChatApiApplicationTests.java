7f96d9ea58ddbba6c8aef7e58410078310d118bb
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-13 14:02:20 +0200

    Test: spring context loads

--- src/test/java/com/chat/chat_api/ChatApiApplicationTests.java
+++ src/test/java/com/chat/chat_api/ChatApiApplicationTests.java
@@ -2,7 +2,9 @@ package com.chat.chat_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev-h2")
 @SpringBootTest
 class ChatApiApplicationTests {
 
