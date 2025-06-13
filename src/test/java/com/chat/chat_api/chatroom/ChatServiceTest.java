ae5e3f2ca709aa1c44049590abe2a5868b2113b5
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-13 08:48:48 +0200

    Test: add ChatService correctly saves chat and returns DTO test

---
+++ src/test/java/com/chat/chat_api/chatroom/ChatServiceTest.java
@@ -0,0 +1,36 @@
+package com.chat.chat_api.chatroom;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.chat.chat_api.chatroom.dto.ChatDTO;
+import com.chat.chat_api.chatroom.dto.CreateChatRequestDTO;
+
+@DisplayName("ChatService unit tests")
+public class ChatServiceTest {
+    
+    @Test
+    @DisplayName("ChatService correctly saves chat and returns DTO")
+    void test_chatService_correctly_saves_chat(){
+
+        String chatName = "Chat1";
+        ChatDTO expectedResult = ChatDTO.toDTO(new Chatroom(chatName, 1L));
+
+        ChatroomRepository repository = mock(ChatroomRepository.class);
+        ChatService chatService = new ChatService(repository);
+
+        CreateChatRequestDTO createChatRequest = new CreateChatRequestDTO(chatName);
+        when(repository.save(any(Chatroom.class))).thenReturn(new Chatroom(chatName, 1L));
+
+        ChatDTO actualResult = chatService.createOrUpdate(createChatRequest);
+        assertThat(actualResult, is(equalTo(expectedResult)));
+
+    }
+}
