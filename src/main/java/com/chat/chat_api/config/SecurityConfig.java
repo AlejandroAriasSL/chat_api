1fd3a13e2aff4e5b188628c4f303e580b43a65bf
AlejandroAriasSL <alejandroasilva02@gmail>
2025-08-27 22:44:07 +0200

    Fix: fix config cors and security

--- src/main/java/com/chat/chat_api/config/SecurityConfig.java
+++ src/main/java/com/chat/chat_api/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.chat.chat_api.config;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,8 +28,10 @@ public class SecurityConfig{
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, @Value("${api.baseurl}") String baseUrl) throws Exception {
         http
+            .cors(cors -> Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2-console/**").authenticated() 
+                .requestMatchers("/chat/**").permitAll()
+                .requestMatchers("/h2-console/**").permitAll() 
                 .requestMatchers(HttpMethod.POST, baseUrl + "/users/auth/**").permitAll() 
                 .anyRequest().authenticated()
             )
